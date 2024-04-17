be7ad45f60c9b67bafa06c1f7abd8bf489cf0ac3
Jashin021 <167222415+Jashin021@users.noreply.github.com>
2024-04-16 19:32:29 -0500

    Delete covid querys.sql

--- covid querys.sql
+++
@@ -1,9 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [continent]
-      ,[location]
-      ,[date]
-      ,[population]
-      ,[new_vaccinations_smoothed]
-      ,[rollingPeople_Vaccinatinated]
-      ,[Percent_of]
-  FROM [Portafolio Proyect].[dbo].[PercentPopulationVaccinated]
